ad206844c54cce5d4e9a42ef576a909a84708614
baiywt <baiywt_gj@163.com>
2020-03-13 16:39:31 +0800

    update scripts

--- build.sh
+++ build.sh
@@ -1,365 +1,270 @@
 #!/bin/bash
+
 set -e
-##########################################
-##
-## Build 3G-iot Linux
-## 
-## Maintainer: Buddy <buddy.zhang@aliyun.com>
-## Maintainer: Leeboby <leeboby@aliyun.com>
-##########################################
-export ROOT=`pwd`
-SCRIPTS=$ROOT/scripts
-export BOOT_PATH
-export ROOTFS_PATH
-export UBOOT_PATH
 
-root_check()
-{
-	if [ "$(id -u)" -ne "0" ]; then
-		echo "This option requires root."
-		echo "Pls use command: sudo ./build.sh"
-		exit 0
-	fi	
-}
+ROOT=`pwd`
+UBOOT="${ROOT}/uboot"
+BUILD="${ROOT}/output"
+LINUX="${ROOT}/kernel"
+EXTER="${ROOT}/external"
+SCRIPTS="${ROOT}/scripts"
+DEST="${BUILD}/rootfs"
+UBOOT_BIN="$BUILD/uboot"
+PACK_OUT="${BUILD}/pack/"
 
-UBOOT_check()
-{
-	for ((i = 0; i < 5; i++)); do
-		UBOOT_PATH=$(whiptail --title "OrangePi Build System" \
-			--inputbox "Pls input device node of SDcard.(/dev/sdc)" \
-			10 60 3>&1 1>&2 2>&3)
-	
-		if [ $i = "4" ]; then
-			whiptail --title "OrangePi Build System" --msgbox "Error, Invalid Path" 10 40 0	
-			exit 0
-		fi
+OS=""
+BT=""
+CHIP=""
+ARCH=""
+DISTRO=""
+ROOTFS=""
+BOOT_PATH=""
+UBOOT_PATH=""
+ROOTFS_PATH=""
+BUILD_KERNEL=""
+BUILD_MODULE=""
 
+SOURCES="CN"
+METHOD="download"
+KERNEL_NAME="linux"
+UNTAR="bsdtar -xpf"
+CORES=$(nproc --ignore=1)
+PLATFORM="$(basename `pwd`)"
 
-		if [ ! -b "$UBOOT_PATH" ]; then
-			whiptail --title "OrangePi Build System" --msgbox \
-				"The input path invalid! Pls input correct path!" \
-				--ok-button Continue 10 40 0	
-		else
-			i=200 
-		fi 
-	done
-}
-
-BOOT_check()
-{
-	## Get mount path of u-disk
-	for ((i = 0; i < 5; i++)); do
-		BOOT_PATH=$(whiptail --title "OrangePi Build System" \
-			--inputbox "Pls input mount path of BOOT.(/media/orangepi/BOOT)" \
-			10 60 3>&1 1>&2 2>&3)
-	
-		if [ $i = "4" ]; then
-			whiptail --title "OrangePi Build System" --msgbox "Error, Invalid Path" 10 40 0	
-			exit 0
-		fi
-
-
-		if [ ! -d "$BOOT_PATH" ]; then
-			whiptail --title "OrangePi Build System" --msgbox \
-				"The input path invalid! Pls input correct path!" \
-				--ok-button Continue 10 40 0	
-		else
-			i=200 
-		fi 
-	done
-}
-
-ROOTFS_check()
-{
-	for ((i = 0; i < 5; i++)); do
-		ROOTFS_PATH=$(whiptail --title "OrangePi Build System" \
-			--inputbox "Pls input mount path of rootfs.(/media/orangepi/rootfs)" \
-			10 60 3>&1 1>&2 2>&3)
-	
-		if [ $i = "4" ]; then
-			whiptail --title "OrangePi Build System" --msgbox "Error, Invalid Path" 10 40 0	
-			exit 0
-		fi
-
-
-		if [ ! -d "$ROOTFS_PATH" ]; then
-			whiptail --title "OrangePi Build System" --msgbox \
-				"The input path invalid! Pls input correct path!" \
-				--ok-button Continue 10 40 0	
-		else
-			i=200 
-		fi 
-	done
-}
-Update_check()
-{
-	MENUSTR="Pls choose SD or EMMC"
-	##########################################
-	Target=$(whiptail --title "OrangePi 3G-iot Build System" \
-    	--menu "$MENUSTR" 10 60 3 --cancel-button Exit --ok-button Select \
-    	"0"  "SD Card" \
-    	"1"  "EMMC" \
-    	3>&1 1>&2 2>&3)
-}
-
-if [ ! -d $ROOT/output ]; then
-    mkdir -p $ROOT/output
-fi
-
-MENUSTR="Welcome to OrangePi Build System. Pls choose Platform."
-##########################################
-OPTION=$(whiptail --title "OrangePi 3G-iot Build System" \
-	--menu "$MENUSTR" 10 60 3 --cancel-button Exit --ok-button Select \
-	"0"  "OrangePi 3G-iot-A" \
-	"1"  "OrangePi 3G-iot-B" \
-	3>&1 1>&2 2>&3)
-
-if [ $OPTION = "0" ]; then
-	export PLATFORM="3g-iot-A"
-	export MTK_PROJECT="hexing72_cwet_lca"
-	export TARGET_PRODUCT=$MTK_PROJECT
-elif [ $OPTION = "1" ]; then
-	export PLATFORM="3g-iot-B"
-	export MTK_PROJECT="hexing72_cwet_kk"
-	export TARGET_PRODUCT=$MTK_PROJECT
+if [[ "${EUID}" == 0 ]]; then
+        :
 else
-	echo -e "\e[1;31m Pls select correct platform \e[0m"
-	exit 0
+	echo " "
+        echo -e "\e[1;31m This script requires root privileges, trying to use sudo \e[0m"
+	echo " "
+        sudo "${ROOT}/build.sh"
+	exit $?
 fi
 
-#cd $ROOT/scripts
-#./Version_Change.sh $PLATFORM
-#cd -
+source "${SCRIPTS}"/lib/general.sh
+source "${SCRIPTS}"/lib/pack.sh
+source "${SCRIPTS}"/lib/compilation.sh
+source "${SCRIPTS}"/lib/distributions.sh
+source "${SCRIPTS}"/lib/build_image.sh
+source "${SCRIPTS}"/lib/platform/rk3399.sh
 
-##########################################
-## Root Password check
-for ((i = 0; i < 5; i++)); do
-	PASSWD=$(whiptail --title "OrangePi Build System" \
-		--passwordbox "Enter your root password. Note! Don't use root to run this scripts" \
-		10 60 3>&1 1>&2 2>&3)
+if [ ! -f $BUILD/.prepare_host ]; then
+        prepare_host
+        touch $BUILD/.prepare_host
+fi
+
+MENUSTR="Welcome to Orange Pi Build System. Pls choose Platform."
+#################################################################
+case "${PLATFORM}" in 
+	"OrangePiH3" | "OrangePiH3_mainline")
+
+		OPTION=$(whiptail --title "Orange Pi Build System" \
+			--menu "${MENUSTR}" 20 80 10 --cancel-button Exit --ok-button Select \
+			"0"  "OrangePi PC Plus" \
+			"1"  "OrangePi PC" \
+			"2"  "OrangePi Plus2E" \
+			"3"  "OrangePi Lite" \
+			"4"  "OrangePi One" \
+			"5"  "OrangePi 2" \
+			"6"  "OrangePi ZeroPlus2 H3" \
+			"7"  "OrangePi Plus" \
+			"8"  "OrangePi Zero" \
+			"9"  "OrangePi R1" \
+			3>&1 1>&2 2>&3)
+
+		case "${OPTION}" in 
+			"0") BOARD="pc-plus" ;;
+			"1") BOARD="pc"	;;
+			"2") BOARD="plus2e" ;;
+			"3") BOARD="lite" ;;
+			"4") BOARD="one" ;;
+			"5") BOARD="2" ;;
+			"6") BOARD="zero_plus2_h3" ;;
+			"7") BOARD="plus" ;;
+			"8") BOARD="zero" ;;
+			"9") BOARD="r1" ;;
+			"*")
+			echo -e "\e[1;31m Pls select correct board \e[0m"
+			exit 0 ;;
+		esac
+
+		if [ "${PLATFORM}" = "OrangePiH3" ]; then
+			TOOLS=$ROOT/toolchain/gcc-linaro-1.13.1-2012.02-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
+			UBOOT_COMPILE="${TOOLS}"
+			KERNEL_NAME="linux3.4.113"
+		elif [ "${PLATFORM}" = "OrangePiH3_mainline" ]; then
+			TOOLS=$ROOT/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+			UBOOT_COMPILE="${TOOLS}"
+			KERNEL_NAME="linux5.3.5"
+		fi
+
+		ARCH="arm"
+		CHIP="sun8iw7p1";
+		CHIP_BOARD="dolphin-p1"
+		;;
+	"OrangePiH6" | "OrangePiH6_Linux4.9" | "OrangePiH6_mainline")
 	
-	if [ $i = "4" ]; then
-		whiptail --title "Note Box" --msgbox "Error, Invalid password" 10 40 0	
+		OPTION=$(whiptail --title "Orange Pi Build System" \
+		        --menu "$MENUSTR" 15 60 5 --cancel-button Exit --ok-button Select \
+		        "0"  "OrangePi 3" \
+		        "1"  "OrangePi Lite2" \
+		        "2"  "OrangePi OnePlus" \
+		        "3"  "OrangePi Zero2" \
+		        3>&1 1>&2 2>&3)
+
+		case "${OPTION}" in 
+			"0") BOARD="3" ;;
+			"1") BOARD="lite2" ;;
+			"2") BOARD="oneplus" ;;
+			"3") BOARD="zero2" ;;
+			"*") 
+			echo -e "\e[1;31m Pls select correct board \e[0m"
+			exit 0 ;;
+		esac
+
+		ARCH="arm64"
+		CHIP="sun50iw6p1"
+		CHIP_BOARD="petrel-p1"
+		CHIP_FILE="${EXTER}"/chips/"${CHIP}"
+		TOOLS=$ROOT/toolchain/gcc-linaro-4.9-2015.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+		UBOOT_COMPILE=$ROOT/toolchain/gcc-linaro-4.9-2015.01-x86_64_aarch64-linux-gnu/gcc-linaro/bin/arm-linux-gnueabi-
+
+		if [ "${PLATFORM}" = "OrangePiH6" ]; then
+			KERNEL_NAME="linux3.10"
+		elif [ "${PLATFORM}" = "OrangePiH6_Linux4.9" ]; then
+			KERNEL_NAME="linux4.9.118"
+		elif [ "${PLATFORM}" = "OrangePiH6_mainline" ]; then
+			KERNEL_NAME="linux5.3.5"
+			TOOLS=$ROOT/toolchain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+			UBOOT_COMPILE="${TOOLS}"
+		fi
+		;;
+	"OrangePiRK3399")
+		OPTION=$(whiptail --title "Orange Pi Build System" \
+		        --menu "$MENUSTR" 15 60 5 --cancel-button Exit --ok-button Select \
+		        "0"  "OrangePi 4" \
+		        "1"  "OrangePi rk3399" \
+		        3>&1 1>&2 2>&3)
+
+		case "${OPTION}" in 
+			"0") BOARD="4" ;;
+			"1") BOARD="rk3399" ;;
+			*) 
+			echo -e "\e[1;31m Pls select correct board \e[0m"
+			exit 0 ;;
+		esac
+
+		ARCH="arm64"
+		CHIP="RK3399"
+		TOOLS=$ROOT/toolchain/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+		KERNEL_NAME="linux4.4.179"
+		;;
+
+	"OrangePi4G-iot")
+		OPTION=$(whiptail --title "Orange Pi Build System" \
+		        --menu "$MENUSTR" 15 60 5 --cancel-button Exit --ok-button Select \
+		        "0"  "OrangePi 4g-iot" \
+		        3>&1 1>&2 2>&3)
+
+		case "${OPTION}" in 
+			"0") BOARD="4g-iot" ;;
+			*) 
+			echo -e "\e[1;31m Pls select correct board \e[0m"
+			exit 0 ;;
+		esac
+
+		ARCH="arm"
+		CHIP="MT6737"
+		#TOOLS=$ROOT/toolchain/arm-linux-androideabi-4.8/bin/arm-linux-androideabi-
+		TOOLS=$ROOT/toolchain/arm-eabi-4.8/bin/arm-eabi-
+		KERNEL_NAME="linux3.18.19"
+		;;
+	"OrangePi3G-iot")
+		OPTION=$(whiptail --title "Orange Pi Build System" \
+		        --menu "$MENUSTR" 15 60 5 --cancel-button Exit --ok-button Select \
+		        "0"  "OrangePi 3g-iot-A" \
+		        "1"  "OrangePi 3g-iot-B" \
+		        3>&1 1>&2 2>&3)
+
+		case "${OPTION}" in 
+			"0") 
+				BOARD="3g-iot-A"
+				MTK_PROJECT="hexing72_cwet_lca"
+				;;
+			"1") 
+				BOARD="3g-iot-B"
+				MTK_PROJECT="hexing72_cwet_kk"
+				;;
+			*) 
+			echo -e "\e[1;31m Pls select correct board \e[0m"
+			exit 0 ;;
+		esac
+
+		ARCH="arm"
+		CHIP="MT6735"
+		TOOLS=$ROOT/toolchain/arm-eabi-4.7/bin/arm-eabi-
+		#TOOLS=$ROOT/toolchain/arm-linux-androideabi-4.7/bin/arm-linux-androideabi-
+		KERNEL_NAME="linux3.4.67"
+		;;
+	*)
+		echo -e "\e[1;31m Pls select correct platform \e[0m"
 		exit 0
-	fi
-
-	sudo -k
-	if sudo -lS &> /dev/null << EOF
-$PASSWD
-EOF
-	then
-		i=10
-	else
-		whiptail --title "OrangePi Build System" --msgbox "Invalid password, Pls input corrent password" \
-			10 40 0	--cancel-button Exit --ok-button Retry
-	fi
-done
-
-echo $PASSWD | sudo ls &> /dev/null 2>&1
-
-## Check cross tools
-if [ ! -d $ROOT/toolchain/gcc-linaro-aarch -o ! -d $ROOT/toolchain/gcc-linaro-aarch/gcc-linaro/arm-linux-gnueabi ]; then
-	cd $SCRIPTS
-#	./install_toolchain.sh
-	cd -
-fi
-
-if [ ! -d $ROOT/output ]; then
-    mkdir -p $ROOT/output
-fi
-
-## prepare development tools
-if [ ! -f $ROOT/output/.tmp_toolchain ]; then
-	cd $SCRIPTS
-	sudo ./Prepare_toolchain.sh
-	touch $ROOT/output/.tmp_toolchain
-	cd -
-fi
+		;;
+esac
 
 MENUSTR="Pls select build option"
-
 OPTION=$(whiptail --title "OrangePi Build System" \
-	--menu "$MENUSTR" 20 60 12 --cancel-button Finish --ok-button Select \
+	--menu "$MENUSTR" 20 60 10 --cancel-button Finish --ok-button Select \
 	"0"   "Build Release Image" \
-	"1"   "Build Preloader and LK" \
-	"2"   "Build Linux" \
-	"3"   "Install Image into EMMC" \
+	"1"   "Build Rootfs" \
+	"2"   "Build Uboot" \
+	"3"   "Build Linux" \
+	"4"   "Build Module only" \
 	3>&1 1>&2 2>&3)
 
-if [ $OPTION = "0" -o $OPTION = "0" ]; then
-	sudo echo ""
-	clear
-	TMP=$OPTION
-	TMP_DISTRO=""
-	MENUSTR="Distro Options"
-	OPTION=$(whiptail --title "OrangePi Build System" \
-		--menu "$MENUSTR" 20 60 3 --cancel-button Finish --ok-button Select \
-		"0"   "Ubuntu Xenial" \
-		"1"   "Debian Jessie" \
-		3>&1 1>&2 2>&3)
+case "${OPTION}" in 
+	"0")
+		select_distro
+		compile_uboot
+		compile_kernel
+		build_rootfs
+		build_image 
 
-        TYPE=$(whiptail --title "OrangePi Build System" \
-                --menu "$MENUSTR" 20 60 3 --cancel-button Finish --ok-button Select \
-                "0"   "Server" \
-                3>&1 1>&2 2>&3)
-
-	if [ ! -f $ROOT/output/boot.img ]; then
-		export BUILD_KERNEL=1
-		cd $SCRIPTS
-		./kernel_compile.sh
-		cd -
-	fi
-	if [ ! -d $ROOT/output/lib ]; then
-		if [ -f $ROOT/output/lib ]; then
-			rm $ROOT/output/lib
-		fi
-		mkdir $ROOT/output/lib
-		export BUILD_MODULE=1
-		cd $SCRIPTS
-		./kernel_compile.sh
-		cd -
-	fi
-#	PRELOADER=$ROOT/output/obj/PRELOADER_OBJ
-#	LK=$ROOT/output/obj/BOOTLOADER_OBJ
-	if [ ! -f $ROOT/output/preloader_$MTK_PROJECT.bin \
-	   	-o ! -f $ROOT/output/lk.bin  \
-		-o ! -f $ROOT/output/logo.bin ]; then
-	    cd $SCRIPTS
-		./uboot_compile.sh
-		cd -
-	fi
-
-	if [ $OPTION = "0" ]; then
-		TMP_DISTRO="xenial"
-        elif [ $OPTION = "1" ]; then
-                TMP_DISTRO="jessie"
-        fi
-
-	if [ $TYPE = "0" ]; then
-		TMP_TYPE="server"
-	elif [ $TYPE = "1" ]; then
-		TMP_TYPE="desktop"
-	fi
-	cd $SCRIPTS
-	DISTRO=$TMP_DISTRO
-        if [ -d $ROOT/output/${DISTRO}_rootfs_$TMP_TYPE ]; then
-                if (whiptail --title "OrangePi Build System" --yesno \
-                        "${DISTRO} rootfs has exist! Do you want use it?" 10 60) then
-                        OP_ROOTFS=0
-                else
-                        OP_ROOTFS=1
-                fi
-                if [ $OP_ROOTFS = "0" ]; then
-                        sudo cp -rf $ROOT/output/${DISTRO}_rootfs_$TMP_TYPE $ROOT/output/tmp
-			if [ -d $ROOT/output/rootfs ]; then
-				sudo rm -rf $ROOT/output/rootfs
-			fi
-			sudo mv $ROOT/output/tmp $ROOT/output/rootfs
-			whiptail --title "OrangePi Build System" --msgbox "Rootfs has build" \
-				10 40 0	--ok-button Continue
-		else
-			sudo rm -rf $ROOT/output/${DISTRO}_rootfs_$TMP_TYPE
-			export DISTRO=$DISTRO
-			export PLATFORM=$PLATFORM
-                        sudo ./00_rootfs_build.sh $DISTRO $PLATFORM $TYPE
-                        sudo ./01_rootfs_build.sh $DISTRO $TMP_TYPE $TARGET_PRODUCT
-		fi
-	else
-		sudo rm -rf $ROOT/output/${DISTRO}_rootfs_$TMP_TYPE
-		export DISTRO=$DISTRO
-		export PLATFORM=$PLATFORM
-                sudo ./00_rootfs_build.sh $DISTRO $PLATFORM $TYPE
-                sudo ./01_rootfs_build.sh $DISTRO $TMP_TYPE $TARGET_PRODUCT
-	fi
-	if [ $TMP = "0" ]; then 
-                 sudo ./build_image.sh $DISTRO $PLATFORM $TYPE
-                whiptail --title "OrangePi Build System" --msgbox "Succeed to build Image" \
-                                10 40 0 --ok-button Continue
-	fi
-	exit 0
-elif [ $OPTION = "1" ]; then
-	cd $SCRIPTS
-	./uboot_compile.sh
-	clear
-	exit 0
-elif [ $OPTION = "2" ]; then
-	export BUILD_KERNEL=1
-	export BUILD_MODULE=1
-	cd $SCRIPTS
-	./kernel_compile.sh $PLATFORM
-	clear
-	exit 0
-elif [ $OPTION = "10" ]; then
-	export BUILD_KERNEL=1
-	cd $SCRIPTS
-	./kernel_compile.sh
-	exit 0
-elif [ $OPTION = "10" ]; then
-	export BUILD_MODULE=1
-	cd $SCRIPTS
-	./kernel_compile.sh
-	exit 0
-elif [ $OPTION = "10" ]; then
-	sudo echo ""
-	clear
-	UBOOT_check
-	clear
-	whiptail --title "OrangePi Build System" \
-			 --msgbox "Burning Image to SDcard. Pls select Continue button" \
-				10 40 0	--ok-button Continue
-	pv "$ROOT/output/${PLATFORM}.img" | sudo dd bs=1M of=$UBOOT_PATH && sync
-	clear
-	whiptail --title "OrangePi Build System" --msgbox "Succeed to Download Image into SDcard" \
-				10 40 0	--ok-button Continue
-	exit 0
-elif [ $OPTION = '11' ]; then
-	clear 
-	Update_check
-	clear
-	if [ $Target = "11" ]; then
-		UBOOT_check
-	fi
-	clear
-	cd $SCRIPTS
-	sudo ./kernel_update.sh $UBOOT_PATH  #$PLATFORM
-	exit 0
-elif [ $OPTION = "11" ]; then
-	sudo echo ""
-	clear 
-	ROOTFS_check
-	clear
-	cd $SCRIPTS
-	sudo ./modules_update.sh $ROOTFS_PATH
-	exit 0
-elif [ $OPTION = "11" ]; then
-	clear
-	Update_check
-	clear
-	if [ $Target = "0" ]; then
-		UBOOT_check
-	fi
-	clear
-	cd $SCRIPTS
-	sudo ./uboot_update.sh $UBOOT_PATH
-	exit 0
-elif [ $OPTION = '3' ]; then
-	clear
-	cd $ROOT/external/SP_Flash_Tool_v5.1644_Linux
-	sudo ./flash_tool.sh 
-	cd -
-	exit 0
-elif [ $OPTION = '10' ]; then
-	clear
-	echo -e "\e[1;31m Updating SDK to Github \e[0m"
-	git push -u origin master
-	exit 0
-elif [ $OPTION = "11" ]; then
-	clear
-	echo -e "\e[1;31m Updating SDK from Github \e[0m"
-	git push origin
-	exit 0
-else
-	whiptail --title "OrangePi Build System" \
-		--msgbox "Pls select correct option" 10 50 0
-	exit 0
-fi
+		whiptail --title "OrangePi Build System" --msgbox "Succeed to build Image" \
+			10 40 0 --ok-button Continue
+		;;
+	"1")
+		select_distro
+		compile_uboot
+		compile_kernel
+		build_rootfs
+		whiptail --title "OrangePi Build System" --msgbox "Succeed to build rootfs" \
+			10 40 0 --ok-button Continue
+		;;
+	"2")	
+		compile_uboot
+		;;
+	"3")
+		compile_kernel
+		compile_module
+		;;
+	"4")
+		compile_module
+		;;
+	"5")
+		[ "${PLATFORM}" = "OrangePiRK3399" ] && uboot_check || boot_check
+		kernel_update
+		;;
+	"6")
+		rootfs_check
+		modules_update
+		;;
+	"7")
+		uboot_check
+		uboot_update
+		;;
+	*)
+		whiptail --title "OrangePi Build System" \
+			--msgbox "Pls select correct option" 10 50 0
+		;;
+esac
